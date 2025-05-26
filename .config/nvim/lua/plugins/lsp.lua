cdb713793f3bcaa9c25077e7ad476f5482d1b5b7
Os24 <oscarhuang950324@gmail.com>
2025-05-26 20:41:57 +0800

    add karabiner

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,18 +2,23 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+    {
+      "williamboman/mason.nvim",
+      config = true,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "lua_ls", "pyright", "tsserver" },
+      },
+    },
   },
   config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "pyright", "tsserver" },
-      handlers = {
-        function(server_name)
-          require("lspconfig")[server_name].setup({})
-        end,
-      },
-    })
+    local lspconfig = require("lspconfig")
+
+    -- 單純直接手動啟用 LSP（避免使用 setup_handlers）
+    lspconfig.lua_ls.setup({})
+    lspconfig.pyright.setup({})
+    lspconfig.tsserver.setup({})
   end,
 }
