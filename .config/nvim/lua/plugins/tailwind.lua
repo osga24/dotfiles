971be21c33a44d9dc7ddb12ed8e12c9db0da5a25
OsGa <84913645+osga24@users.noreply.github.com>
2025-09-25 14:34:32 +0800

    好醜的 config

---
+++ .config/nvim/lua/plugins/tailwind.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes = {
+            "html",
+            "css",
+            "scss",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+            "svelte",
+          },
+        },
+      },
+    },
+  },
+}
