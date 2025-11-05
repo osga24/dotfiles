5c1927c348c11af9d9ac32ed1a7430cdd57f6bed
OsGa <84913645+osga24@users.noreply.github.com>
2025-11-05 12:11:32 +0800

    Delete nvim directory

--- nvim/lua/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "storm",
-      transparent = true, -- ✅ 主題不要畫背景
-    },
-  },
-}
--- nvim/lua/plugins/flutter.lua
+++
@@ -1,7 +0,0 @@
--- ~/.config/nvim/lua/plugins/flutter.lua
-return {
-  "akinsho/flutter-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  ft = "dart",
-  opts = {},
-}
